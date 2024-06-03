d7f9287a5114415b1e45a983786a7e9535846d12
chrisVan-G <chrisvaec@gmail.com>
2024-06-03 15:20:23 -0500

    Introducción a JavaScript

---
+++ api.js
@@ -0,0 +1,3 @@
+// Apendiendo javaScript
+console.log("%cHola Mundo", "color: orangered; font-size: 100px");
+//use un código CSS válido para diseñar las palabras que desee que aparezcan en la consola.
---
+++ operadores.js
@@ -0,0 +1,25 @@
+// Manejo de operadores
+
+console.log("Resultado operdores aritméticos y de comparación")
+
+let a = 12;
+let b = 33;
+let c = 16;
+
+console.log();
+console.log(a + b + c);
+console.log(a + b + c + 21 + 100);
+console.log(b - a);
+console.log(a * c);
+console.log(b / a);
+console.log(a > c);
+console.log(c < b);
+console.log( a == c);
+console.log();
+
+console.log("Resultado operdores lógicos");
+
+console.log();
+console.log(a == c || c-4 == a);
+console.log(b != c);
+console.log(a && b == c);
