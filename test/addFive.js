56fc3abb82387164ac92775af95ccfa0012aff1e
chrisVan-G <chrisvaec@gmail.com>
2024-06-26 15:30:12 -0500

    Manejo de pruebas unitarias con jest

---
+++ test/addFive.js
@@ -0,0 +1,12 @@
+function addFive(val) {
+  return val + 5;
+}
+
+module.exports = addFive;
+
+// Comandos
+// npm install --save-dev jest?
+// npm init -y
+// Para poder ejecutar los archivos de esta carpeta se debe instalar jest y hacer los ajustes
+// en el archivo package.json => "test": "jest"
+// para ejecutar el código utilizo npm run test
