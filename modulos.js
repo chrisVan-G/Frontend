5b54002abb92645e9dae2e3f88e509298d06d325
chrisVan-G <chrisvaec@gmail.com>
2024-06-30 12:50:34 -0500

    Manejo de modulos con JS

--- modulos.js
+++ modulos.js
@@ -1,6 +1,10 @@
+import { suma } from './modulos_2';
+
+console.log('Archivo de modulos');
 var saludo = 'Hola';
 var lugar = 'Mundo';
 function saludar() {
   console.log(`${saludo} ${lugar}`);
 }
 saludar();
+suma(2, 2);
