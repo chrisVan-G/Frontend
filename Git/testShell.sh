ce110f49cbc0590e7cf950f80294e9fb66aa69f2
chrisVan-G <chrisvaec@gmail.com>
2024-07-04 14:21:15 -0500

    Manejo de comandos básicos Unix

---
+++ Git/testShell.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Hola Mundo, vamos a aprender a usar comandos!"
+
+# Hoja de trucos para crear scripts en Bash
+# https://devhints.io/bash
+# Hoja de referencia de grep de GNU
+# https://devhints.io/grep
+# Hoja de trucos de comandos Unix
+# https://cheatography.com/jluis/cheat-sheets/bash-and-unix-commands/
+# Hoja de trucos de Vim
+# https://vim.rtorr.com/
