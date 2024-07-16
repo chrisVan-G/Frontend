d1d5b4e76e15f11c8a715f256c6d2632062a9fa1
chrisVan-G <chrisvaec@gmail.com>
2024-07-15 20:38:47 -0500

    Repaso variables con JS

--- api.js
+++ api.js
@@ -3,3 +3,5 @@
 console.log('%cHola Mundo!', 'color: orangered; font-size: 100px');
 
 //use un código CSS válido para diseñar las palabras que desee que aparezcan en la consola.
+
+// Curso de Jhon Mircha javaScript sigue el video 4
