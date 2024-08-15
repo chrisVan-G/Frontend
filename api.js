c580aba1193e3755a8c4353550237dba30ef88ee
chrisVan-G <chrisvaec@gmail.com>
2024-08-15 15:12:43 -0500

    Actualización archivo api.js

--- api.js
+++ api.js
@@ -4,4 +4,4 @@ console.log('%cHola Mundo!', 'color: orangered; font-size: 100px');
 
 //use un código CSS válido para diseñar las palabras que desee que aparezcan en la consola.
 
-// Curso de Jhon Mircha javaScript sigue el video 4
+// Curso de Jhon Mircha javaScript sigue el video número 4
